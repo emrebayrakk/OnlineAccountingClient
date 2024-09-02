3851ad9f1132d367899c7a67b7857f144efdd042
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2024-09-02 18:35:29 +0300

    adminlte theme added project and first commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { importProvidersFrom } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      RouterModule.forRoot([
+        {
+          path:"",
+          loadComponent: ()=> import("./app/ui/components/layouts/layouts.component").then(c=>c.LayoutsComponent),
+          children:[
+            {
+              path:"",
+              loadComponent: ()=> import("./app/ui/components/blank/blank.component").then(c=>c.BlankComponent),
+            }
+          ]
+        },
+        {
+          path:"login",
+          loadComponent:
+          ()=> import("./app/ui/components/auth/login/login.component").then(c=>c.LoginComponent)
+        }
+      ])
+    )
+  ]
+})
